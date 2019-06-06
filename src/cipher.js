3b51c5f4b9e8c36a13469945ea618d087a839b76
YaninaContreras <yanicontreras.23@gmail.com>
2019-06-06 16:32:50 -0500

    dandole estilo al screen1

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,13 @@
 window.cipher = {
   encode: (offset, string) => {
+  // let solved ="";
+  //   for (let i=0; i<string.length; i++)
+  //   const asciiNum = string[i].charCodeAt(
+  //     if(asciiNum>=65 && asciiNum<=90){
+  //       solved+= String.fromCharCode(asciiNum + offset))
+  //     }
+  //   } return solved;
+
     /* Acá va tu código que cifra*/
   },
   decode: (offset, string) => {
--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 /* Acá va tu código */
 /*llamando los inputs*/
+
+
+
+const contraseña = document.getElementById('contraseña');
+const go1 = document.getElementById('go1');
+let texto = document.getElementById('texto');
+let codigo = document.getElementById('codigo');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const go = document.getElementById('go2');
+let resultado = document.getElementById('reultado');
+const volver = document.getElementById('volver');
+const salir = document.getElementById('salir');
+const clave = "laboratoria".toUpperCase();
+console.log (clave);
+
+
+go1.addEventListener("click",() => {
+	const pass = contraseña.value;
+	
+	if (pass == clave) {  
+		alert("HERE IS");
+	} else {
+		alert("intenta de nuevo");
+	}
+});
+  screen2.style.display = 'none';
+  screen3.style.display = 'none';
+
+
+
+
+
